94f155551161a9388545decd0a48b404e67b7267
Esneider Ospina <sneuder8@gmail.com>
2022-04-14 18:23:22 -0500

    Navbar starting

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <nav className={style.background}>
+      <div className={style.branchContainer}>
+        <p>
+          Sne<span>uder</span>
+        </p>
+      </div>
+
+      <ul className={style.navItemsContainer}>
+        <li className={style.navItems}>About Me</li>
+        <li className={style.navItems}>Projects</li>
+        <li className={style.navItems}>My CV</li>
+        <li className={style.navItems}>Skills</li>
+        <li className={style.navItems}>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
