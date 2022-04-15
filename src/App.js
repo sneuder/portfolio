362a6687cc91d8e3968c1d93971f287f3d4c179c
Esneider Ospina <sneuder8@gmail.com>
2022-04-14 19:28:45 -0500

    Basics of about_me component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar/navbar";
+import AboutMe from "./components/about_me/about_me";
 
 function App() {
   return (
     <div className="app">
      <Navbar />
+     <AboutMe />
     </div>
   );
 }
---
+++ src/components/about_me/about_me.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import style from "./about_me.module.scss";
+
+const AboutMe = () => {
+  return (
+    <div className={style.background}>
+      <div className={style.containerAboutMe}>
+        <div className={style.subContainerAboutMe}>
+          <h2 className={style.header}>Hi,</h2>
+          <h2 className={style.header}>
+            I'm <span className={style.headerDecoration}>Esneider</span>
+          </h2>
+          <h3 className={style.subHeader}>Full Stack Web Developer</h3>
+          <p className={style.text}>
+            Hey, are looking for a web developer to build your{" "}
+            <span>Brand and grow your business?</span> Let's shake hands with
+            me.
+          </p>
+          <div className={style.containerButtons}>
+            <FontAwesomeIcon className={style.linkedinIcon} icon={faLinkedin} />
+            <button className={style.hireMeButton}>Hire me</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.containerImage}></div>
+    </div>
+  );
+};
+
+export default AboutMe;
