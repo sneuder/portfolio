7f27fe7a8cdcd7898580604401692899b3c128ec
Esneider Ospina <sneuder8@gmail.com>
2022-04-16 11:57:02 -0500

    Building project section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/navbar";
 import AboutMe from "./components/about_me/about_me";
+import Projects from "./components/projects/projects";
 
 function App() {
   return (
     <div className="app">
      <Navbar />
      <AboutMe />
+     <Projects />
     </div>
   );
 }
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import style from "./projects.module.scss";
+
+const Projects = () => {
+  return (
+    <div className={style.background}>
+      <div className={style.headerContainer}>
+        <h1 className={style.header}>
+          Some of my <span className={style.headerDecoration}>work</span>
+        </h1>
+        <p className={style.text}>
+          As a full stack web developer I have completing some personal projects
+        </p>
+        <div className={style.linkRepositoriesContainer}>
+          <FontAwesomeIcon className={style.gitHubIcon} icon={faGithubSquare} />
+          <p>Check repositories</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
