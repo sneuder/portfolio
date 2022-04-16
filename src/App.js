031e7efc2d73945a276e9c8f72a0037df402ee17
Esneider Ospina <sneuder8@gmail.com>
2022-04-16 18:27:28 -0500

    Adding the basics for contact components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/navbar";
 import AboutMe from "./components/about_me/about_me";
 import Projects from "./components/projects/projects";
 import Skills from "./components/skills/skills";
+import Contact from "./components/contact/contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <AboutMe />
      <Projects />
      <Skills />
+     <Contact />
     </div>
   );
 }
---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./contact.module.scss";
+
+const Contact = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+export default Contact;
