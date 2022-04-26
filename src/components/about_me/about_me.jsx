47fb966273fb3d903261d83a2d8be72554faaebd
Esneider Ospina <sneuder8@gmail.com>
2022-04-26 11:54:55 -0500

    Function for Hire me button

--- src/components/about_me/about_me.jsx
+++ src/components/about_me/about_me.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { NavHashLink as Link } from "react-router-hash-link";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import style from "./about_me.module.scss";
 
@@ -17,23 +19,41 @@ const AboutMe = () => {
           <h3 className={style.subHeader}>Full Stack Web Developer</h3>
           <p className={style.text}>
             Hey, are looking for a web developer to build your{" "}
-            <span className={style.textDecoration}>Brand and grow your business?</span> Let's shake hands with
-            me.
+            <span className={style.textDecoration}>
+              Brand and grow your business?
+            </span>{" "}
+            Let's shake hands with me.
           </p>
           <div className={style.containerButtons}>
-            <a href="https://www.linkedin.com/in/esneider-fullstackdeveloper/" target="_blank">
-              <FontAwesomeIcon className={style.linkedinIcon} icon={faLinkedin} />
+            <a
+              href="https://www.linkedin.com/in/esneider-fullstackdeveloper/"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                className={style.linkedinIcon}
+                icon={faLinkedin}
+              />
             </a>
-            <div className={style.button}>
-              <p className={style.buttonText}>Hire me</p>
-              <FontAwesomeIcon className={style.arrowIcon} icon={faArrowRight}/>
-            </div>
+            <Link smooth to="#contact">
+              <div className={style.button}>
+                <p className={style.buttonText}>Hire me</p>
+
+                <FontAwesomeIcon
+                  className={style.arrowIcon}
+                  icon={faArrowRight}
+                />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
 
       <div className={style.containerImage}>
-        <img className={style.image} src={require("../../assets/aboutme.png")} alt="about me" />
+        <img
+          className={style.image}
+          src={require("../../assets/aboutme.png")}
+          alt="about me"
+        />
       </div>
     </div>
   );
