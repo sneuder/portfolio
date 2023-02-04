82ac8f622e1b736f6126dce0f96d3b1dd95b8f15
Esneider Ospina Oviedo <sneuder@torneos.gg>
2023-02-04 14:44:18 -0500

    feat: logo, basics and navbar config

---
+++ src/common/confirmationCard/index.jsx
@@ -0,0 +1,12 @@
+const ConfirmationCard = () => {
+  return (
+    <div>
+      <div>
+        <p>You message has been sent</p>
+        <button>Accept</button>
+      </div>
+    </div>
+  )
+}
+
+export default ConfirmationCard
--- src/components/navbar/mobile/index.jsx
+++ src/components/navbar/mobile/index.jsx
@@ -1,12 +1,17 @@
+import useGetStates from '../../../hooks/useGetStates'
 import style from './index.module.scss'
 import NavItems from '../../../common/navItems'
 import Branch from '../../../common/branch'
 
 const MobileNavbar = () => {
+  const { modal } = useGetStates()
+
   return (
-    <div className={style.mobileNavbarContainer}>
-      <Branch />
-      <NavItems />
+    <div className={`${style.backgroundMobileNavbar} ${!modal ? style.hideNavbarMobile : style.showNavbarMobile}`}>
+      <div className={style.mobileNavbarContainer}>
+        <Branch />
+        <NavItems />
+      </div>
     </div>
   )
 }
