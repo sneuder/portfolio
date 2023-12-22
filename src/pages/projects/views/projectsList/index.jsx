83370d727d0675cd700db555f17bf201cf549555
Sneuder <sneuder8@gmail.com>
2023-12-22 01:07:56 +0000

    feat: use of lazy loading in components and image card project

--- src/pages/projects/views/projectsList/index.jsx
+++ src/pages/projects/views/projectsList/index.jsx
@@ -1,8 +1,12 @@
+import React, { Suspense } from 'react'
 import style from './index.module.scss'
-
-import CardProject from '../../../../common/cardProject'
 import Vertical from '../../../../common/layouts/vertical'
 import GoTo from '../../../../common/goTo'
+
+// Lazy load CardProject component
+const LazyCardProject = React.lazy(() =>
+  import('../../../../common/cardProject')
+)
 
 const ProjectsList = () => {
   const { projects } = require('../../../../assets/data/index.json')
@@ -18,9 +22,11 @@ const ProjectsList = () => {
       <GoTo instruction={goToInstruction} />
 
       <div className={style.containerProjects}>
-        {projects.map((project) => (
-          <CardProject project={project} key={project.id} />
-        ))}
+        <Suspense fallback={<div>Loading...</div>}>
+          {projects.map((project) => (
+            <LazyCardProject project={project} key={project.id} />
+          ))}
+        </Suspense>
       </div>
     </Vertical>
   )
